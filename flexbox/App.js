32d7768d0be920812a739e64679c818423ea1470
anojaM <anojamadusanka7@gmail.com>
2020-05-28 20:51:59 +0530

    Navigation

--- flexbox/App.js
+++ flexbox/App.js
@@ -1,36 +1,58 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+// In App.js in a new project
 
-export default function App(){
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.block}>
-      <View style={styles.containerOne}>
-
-      </View>
-      <View style={styles.containerTwo}>
-
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to details screen"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  block : {
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center'
-  },
-  containerOne : {
-    flex:1,
-    height:100,
-    backgroundColor: 'yellow',
-    
-  },
-  containerTwo : {
-    flex:4,
-    backgroundColor: 'blue',
-    height:100,
-  }
-});
+function DetailsScreen({navigation}) {
+  return(
+    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+      <Text>
+        DetailsScreen
+      </Text>
+      <Button title="Go to details screen again..." onPress={()=>navigation.push("Details")}/>
+      <Button title="Go to home" onPress={()=>navigation.navigate("Home")}/>
+      <Button title="Go back" onPress={()=>navigation.goBack()}/>
+      <Button title="Go to the first screen" onPress={()=>navigation.popToTop()}/>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: '#009387',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyles: {
+            fontWeight: 'bold',
+          }
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          title:'Overview'
+        }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
